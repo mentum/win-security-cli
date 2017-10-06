9dfe2820a293f605228429e19f4b323d8c4238ef
EFF <frxpoulin@gmail.com>
2017-10-06 08:54:19 -0400

    added password check

--- dpapi_cli/Program.cs
+++ dpapi_cli/Program.cs
@@ -1,9 +1,52 @@
-﻿using System;
+
+using System;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Security.Permissions;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.ConstrainedExecution;
+using System.Security;
 using System.Text;
 
 namespace dpapi_cli {
     class Program {
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool LogonUser(String lpszUsername, String lpszDomain, String lpszPassword,
+            int dwLogonType, int dwLogonProvider, out IntPtr phToken);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public extern static bool CloseHandle(IntPtr handle);
+        
+        private static string pCheck() {
+            IntPtr tokenHandle;
+            string userName;
+
+            userName = Environment.UserName;
+
+            const int LOGON32_PROVIDER_DEFAULT = 0;
+            //This parameter causes LogonUser to create a primary token.
+            const int LOGON32_LOGON_INTERACTIVE = 2;
+
+            // Call LogonUser to obtain a handle to an access token.
+            bool loginSuccess = LogonUser(userName, null, "",
+                LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT,
+                out tokenHandle);
+
+            if(tokenHandle != IntPtr.Zero) {
+                CloseHandle(tokenHandle);
+            }
+
+            int errorCode = Marshal.GetLastWin32Error();
+
+            if (loginSuccess || errorCode == 1327)
+            {
+                return "false";
+            } else {
+                return "true";
+            }
+        }
 
         private static string Encrypt() {
             var inputString = Console.ReadLine();
@@ -51,6 +94,14 @@ namespace dpapi_cli {
                         output = Decrypt();
                     } catch {
                         Error("Decryption Error.");
+                    }
+                    break;
+
+                case "p":
+                    try {
+                        output = pCheck();
+                    } catch {
+                        Error("Password check error");
                     }
                     break;
             }
